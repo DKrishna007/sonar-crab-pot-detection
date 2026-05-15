d938064d2cc7e36d2d4309f440827099eb9908e9
Krishna <75413298+DKrishna007@users.noreply.github.com>
2026-05-15 13:44:12 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'sonar_perception'
+
+setup(
+      name=package_name,
+      version='1.0.0',
+      packages=find_packages(exclude=['test']),
+      data_files=[
+                ('share/ament_index/resource_index/packages',
+                             ['resource/' + package_name]),
+                ('share/' + package_name, ['package.xml']),
+                (os.path.join('share', package_name, 'launch'),
+                             glob('launch/*.launch.py')),
+                (os.path.join('share', package_name, 'config'),
+                             glob('config/*.yaml')),
+      ],
+      install_requires=['setuptools'],
+      zip_safe=True,
+      maintainer='Krishna Digamarthi',
+      maintainer_email='shivasaikrishna23@gmail.com',
+      description='Side-scan sonar crab-pot detection with ROS 2',
+      license='MIT',
+      tests_require=['pytest'],
+      entry_points={
+                'console_scripts': [
+                              'packet_stitcher = sonar_perception.packet_stitcher:main',
+                              'detector        = sonar_perception.detector:main',
+                              'gnss_imu_fusion = sonar_perception.gnss_imu_fusion:main',
+                ],
+      },
+)
